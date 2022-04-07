5b0cea95f9c1b6ee256d2d417365bc2b1002045d
tndus77 <tndus77@sookmyung.ac.kr>
2022-04-07 22:48:34 +0900

    [feat] 채팅방 입장, +아이콘 추가, 채팅 filter 틀만 구성(#5)

--- src/pages/chetting/ChettingMain.tsx
+++ src/pages/chetting/ChettingMain.tsx
@@ -3,11 +3,8 @@ import { SafeAreaView, View, StyleSheet, Text, Dimensions, TouchableOpacity, Fla
 import { TextInput } from 'react-native-gesture-handler';
 import { Title } from 'react-native-paper';
 import Ionicons from 'react-native-vector-icons/Ionicons'
-import Feather from 'react-native-vector-icons/Feather'
-import { color } from 'react-native-elements/dist/helpers';
-import { colors } from 'react-native-elements';
 
-export default function ChettingMain(navigation) {
+export default function ChettingMain({ route, navigation }) {
     const ListTab = [
         {
             tabname: 'MY'
@@ -41,7 +38,7 @@ export default function ChettingMain(navigation) {
     ]
     const [status, setStatus] = useState('MY')
     const [datalist, setDatalist] = useState(data)
-
+    const [result, setResult] = useState<any>([])
     const setStatusFilter = status => { //status는 listTab.tabname
         if(status !== 'MY') {
             setDatalist([...data.filter(v => v.roomName === status)])
@@ -54,7 +51,9 @@ export default function ChettingMain(navigation) {
     const renderItem = ({item, index}) => {
         return(     
             <TouchableOpacity 
-             onPress={() => {navigation.navigate('ChattingRoom')}}>
+             onPress={() => {
+                 navigation.navigate('ChattingRoom', { 
+                     roomName: item.roomName})}}>
                 <View key={index} style={styles.itemContainer}>
                     <Text style={styles.roomName}>{item.roomName}</Text>
                     <Text style={styles.chat}>{item.chat}</Text>
@@ -66,11 +65,26 @@ export default function ChettingMain(navigation) {
     
     return (
         <SafeAreaView style={styles.container}>
-            <Title style={styles.title}>대화</Title>
-            <TouchableOpacity
-             onPress={() => {}}>
-                <Ionicons name=''/>
-            </TouchableOpacity>
+            <View style={styles.iconContainer}>
+                <View/>
+                <Title style={styles.title}>대화</Title>
+                    <TouchableOpacity
+                    onPress={() => {}}>
+                        <Ionicons style={styles.icon}name='add-circle-outline' size={28} color='#555' />
+                    </TouchableOpacity>
+            </View>
+            <View style={styles.textInputRow}>
+                <TextInput style={styles.textSearch} onEndEditing={() => onPressSearch(setResult)} />
+                <TouchableOpacity onPress={() => onPressSearch(setResult)}>
+                    <Ionicons name='search' size={25} color='#555' />
+                </TouchableOpacity>
+            </View>
+            {/* <FlatList
+                data={result}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+                contentContainerStyle={{ paddingHorizontal: 20 }}
+            /> */}
             <View style={styles.listTab}>
                 {
                     ListTab.map(v => (
@@ -85,24 +99,49 @@ export default function ChettingMain(navigation) {
             
             <FlatList
              data = {datalist}
-             keyExtractor={(v, i) => i.toString()}
+             keyExtractor={(item) => item.toString()}
              renderItem={renderItem}
             />
     </SafeAreaView>
     
     )}
 
+function onPressSearch(setResult) {
+    setResult([{
+        id: '1',
+        roomName: '시스템종합설계',
+        chat: '팀원 구해요',
+        people: '22'
+    }])
+}
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingHorizontal: 10,
         backgroundColor: '#fff'
     },
-    listTab: {
-        padding: 15,
+    textInputRow: {
+        backgroundColor: '#f5f5f5',
+        marginHorizontal: 20,
+        marginVertical: 10,
+        borderRadius: 10,
         flexDirection: 'row',
-        alignSelf: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 15
+    },
+    textSearch: {
+        flex: 1,
+        paddingVertical: 7
+    },
+    listTab: {
+        padding: 5,
+        flexDirection: 'row',
+        alignSelf: 'center', 
         marginBottom: 20
+    },
+    iconContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around'
     },
     btnTab: {
         width: Dimensions.get('window').width / 2.0,
@@ -115,9 +154,15 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 16,
         fontWeight: 'bold',
-        alignSelf: 'center',
         paddingVertical: 20,
         color: '#003087',
+        alignSelf: 'center'
+    },
+    icon: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        padding: 20
     },
     textInput: {
         width: '100%',
@@ -143,6 +188,7 @@ const styles = StyleSheet.create({
         padding: 15
     },
     roomName: {
+        fontWeight: 'bold',
         color: '#003087',
         fontSize: 17,
     },
--- src/pages/chetting/ChettingNavigator.tsx
+++ src/pages/chetting/ChettingNavigator.tsx
@@ -1,10 +1,12 @@
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
 import ChettingMain from './ChettingMain'
+import ChattingRoom from './ChattingRoom'
 
 export default function ChettingNavigator() {
     const Stack = createStackNavigator()
     return <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name='ChettingMain' component={ChettingMain} />
+        <Stack.Screen name='ChattingRoom' component={ChattingRoom} />
     </Stack.Navigator>
 }
